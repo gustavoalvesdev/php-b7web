daba99f636a69c7252377cdbea3fe63ed0bb0d81
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2025-06-23 00:58:33 -0300

    Tipos Primitivos no PHP

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php 
+
+/* 
+    String; // TEXTO
+    Integer; //  NÚMEROS INTEIROS (SEM VÍRGULA)
+    Float; // NÚMEROS COM VÍRGULA 
+    Boolean; // VERDADEIRO OU FALSO / LÓGICO
+*/
+
+// String
+$nome = "Gustavo";
+
+// Integer
+$quantidadeAlunos = 10;
+
+// Float
+$precoArroz = 10.25;   
+
+// Boolean
+$choveuHoje = true;
+
+echo "Preço do Arroz: " . $precoArroz;
+echo "<br>";
+echo "Nome do Aluno: " . $nome;
+echo "<br>";
+echo "Quantidade de Alunos: " . $quantidadeAlunos;
+echo "<br>";
+echo "Choveu Hoje: " . ($choveuHoje ? 'Sim' : 'Não');
+ 
