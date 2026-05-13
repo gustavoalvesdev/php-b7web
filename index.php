6afb25aef1fef0a5e9c4ac133a7d3f4d77e15cac
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2026-05-13 19:54:20 -0300

    Decisões Inteligentes: Introdução às Condicionais em PHP

--- index.php
+++ index.php
@@ -1,21 +1,10 @@
 <?php 
 
-$texto = 'Pera, uva maçã e salada mista';
+$nome = 'Maria';
+$idade = 22;
 
-$posicaoPalavraUva = strpos($texto, 'uva');
-$posicaoPalavraPera = strpos($texto, 'Pera');
-
-echo "A palavra uva começa na posição $posicaoPalavraUva e a palavra pera começa na poosição $posicaoPalavraPera";
-
-$parteString = substr($texto, 6, 12);
-
-echo '<br>';    
-
-echo 'Parte da String: ' . $parteString;
-
-echo '<br>';
-
-$tamanhoPalavraPera = strlen('pera');
-
-$pera = substr($texto, $posicaoPalavraPera, $tamanhoPalavraPera);
-echo 'Pera: ' . $pera;
+if ($idade >= 18) {
+    echo $nome . ', você pode acessar o sistema';
+} else {
+    echo $nome . ', você não pode acessar o sistema';
+}
